30ecaa0910ef9635372b42d04c440008fdf6953e
NAOUI1 <121251825+NAOUI1@users.noreply.github.com>
2022-12-22 20:56:55 +0100

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=admin 
+export POSTGRES_PASSWORD=said654321
+export POSTGRES_HOST= localhost
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=udagram-said
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=Qeiv1RREIWBsHXMqdLtxkE2kLrX50h242IhXj5Yj
+export URL=http://localhost:8100
